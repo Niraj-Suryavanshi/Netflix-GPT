1d50f6cda41d41248c93a691dda79e7ef7e9f3f8
Sunil Suryavanshi <52320329+Niraj-Suryavanshi@users.noreply.github.com>
2024-02-26 19:34:52 +0530

    cra & tailwind

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-700">
+      {" "}
+      Namaste Everyone ! Lets build netflix gpt
+    </div>
+  );
+}
+
+export default App;
