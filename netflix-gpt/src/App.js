66856a267c7956e00ebb11629f1dc2ca3b44e9f4
Sunil Suryavanshi <52320329+Niraj-Suryavanshi@users.noreply.github.com>
2024-02-27 17:12:28 +0530

    singin/sign up form && Login/header

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,12 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-700">
-      {" "}
-      Namaste Everyone ! Lets build netflix gpt
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
